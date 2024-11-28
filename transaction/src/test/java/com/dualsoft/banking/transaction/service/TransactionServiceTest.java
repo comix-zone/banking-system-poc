aaa172d15d07974d793545ec07b617b1eeab85ee
n.carovic <n.carovic@logifuture.com>
2024-11-28 13:39:20 +0100

    Add correlation id to test for concurrency

--- transaction/src/test/java/com/dualsoft/banking/transaction/service/TransactionServiceTest.java
+++ transaction/src/test/java/com/dualsoft/banking/transaction/service/TransactionServiceTest.java
@@ -16,7 +16,6 @@ import java.util.UUID;
 import java.util.concurrent.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class TransactionServiceTest {
@@ -25,7 +24,7 @@ public class TransactionServiceTest {
 
     @Test
     public void testConcurrentMoneyTransfer() throws InterruptedException, ExecutionException, ExecutionException, TransactionDoubleInitializationException, TransactionUserNotFoundException {
-       //Init wallet
+        //Init wallet
         UUID fromAccountId = UUID.randomUUID();
         UUID toAccountId1  = UUID.randomUUID();
         UUID toAccountId2  = UUID.randomUUID();
@@ -54,11 +53,13 @@ public class TransactionServiceTest {
 
         BigDecimal transferAmount = new BigDecimal("50.00");
         String currency = "RSD";
-
+        var correlationId1 = UUID.randomUUID();
+        var correlationId2 = UUID.randomUUID();
         // Prepare request DTOs
         TransferMoneyRequestDTO request1 = new TransferMoneyRequestDTO();
         request1.setFromAccountId(fromAccountId);
         request1.setToAccountId(toAccountId1);
+        request1.setCorrelationId(correlationId1);
         request1.setAmount(transferAmount);
         request1.setCurrency(currency);
 
@@ -66,6 +67,7 @@ public class TransactionServiceTest {
         request2.setFromAccountId(fromAccountId);
         request2.setToAccountId(toAccountId2);
         request2.setAmount(transferAmount);
+        request2.setCorrelationId(correlationId2);
         request2.setCurrency(currency);
 
         // Executor service for concurrent execution
@@ -74,22 +76,16 @@ public class TransactionServiceTest {
         // Create tasks
         Callable<List<TransferMoneyResponseDTO>> task1 = () -> service.transferMoney(request1);
         Callable<List<TransferMoneyResponseDTO>> task2 = () -> service.transferMoney(request2);
-        Callable<List<TransferMoneyResponseDTO>> task3 = () -> service.transferMoney(request1);
-        Callable<List<TransferMoneyResponseDTO>> task4 = () -> service.transferMoney(request2);
 
 
         // Submit tasks for concurrent execution
         List<Future<List<TransferMoneyResponseDTO>>> futures = new ArrayList<>();
         futures.add(executor.submit(task1));
         futures.add(executor.submit(task2));
-        futures.add(executor.submit(task3));
-        futures.add(executor.submit(task4));
 
         // Wait for results
         List<TransferMoneyResponseDTO> result1 = futures.get(0).get();
         List<TransferMoneyResponseDTO> result2 = futures.get(1).get();
-        List<TransferMoneyResponseDTO> result3 = futures.get(1).get();
-        List<TransferMoneyResponseDTO> result4 = futures.get(1).get();
 
 
         executor.shutdown();
@@ -99,18 +95,8 @@ public class TransactionServiceTest {
         var balanceTo2  = service.getUserBalance(toAccountId2);
 
         // Assert results
-        assertEquals(9800,balanceFrom.getBalance().intValue(), "balance of from account does not match");
-        assertEquals(100, balanceTo1.getBalance().intValue(), "balance of to1 account does not match");
-        assertEquals(100, balanceTo2.getBalance().intValue(), "balance of to2 account does not match");
-        // Assert results
-//        assertTrue(result1 != null || result2 != null, "At least one transaction should succeed");
-//        if (result1 != null && result2 != null && result3 != null && result4 != null) {
-//            assertEquals(transferAmount, result1.get(0).getBalance());
-//            assertEquals(transferAmount, result2.get(0).getBalance());
-//            assertEquals(transferAmount, result3.get(0).getBalance());
-//            assertEquals(transferAmount, result4.get(0).getBalance());
-//        } else {
-//            System.out.println("One transaction failed due to concurrency issues or insufficient balance.");
-//        }
+        assertEquals(9900,balanceFrom.getBalance().intValue(), "balance of from account does not match");
+        assertEquals(50, balanceTo1.getBalance().intValue(), "balance of to1 account does not match");
+        assertEquals(50, balanceTo2.getBalance().intValue(), "balance of to2 account does not match");
     }
 }
